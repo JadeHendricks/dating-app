fd337362a3ea68552eb270ff4268489006060a43
Jade <hendricksjade@gmail.com>
2023-11-21 20:13:00 +0200

    add(cors): adding in cors compatibility to our API

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(CorsPolicyBuilder => CorsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
