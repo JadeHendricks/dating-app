0905086f2b8b6c8b591074bf186a11f5456d74b4
Jade <hendricksjade@gmail.com>
2023-11-24 11:38:21 +0200

    add(auth): adding in a login endpoint, implementing a token service and creating/signing the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,9 +1,9 @@
-﻿using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,9 +13,11 @@ public class AccountController : BaseApiController
 {
     
     private readonly DataContext _context;
+    private readonly ITokenService _tokenService;
 
-    public AccountController(DataContext context)
+    public AccountController(DataContext context, ITokenService tokenService)
     {
+        _tokenService = tokenService;
         _context = context;
     }
 
@@ -36,6 +38,28 @@ public class AccountController : BaseApiController
         await _context.SaveChangesAsync();
 
         return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("Invalid Username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < ComputeHash.Length; i++) {
+            if (ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+
+        return new UserDto 
+        {
+            Username = user.UserName,
+            Token = _tokenService.CreateToken(user)
+        };
     }
 
     private async Task<bool> UserExists(string username)
