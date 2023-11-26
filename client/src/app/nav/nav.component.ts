7a331e1996a33306f0378cb0922605f84662b582
Jade <hendricksjade@gmail.com>
2023-11-26 17:58:41 +0200

    add(persisting): Persisting the users login session and destryoing unused observables when they are not in use.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
@@ -8,25 +10,25 @@ import { AccountService } from '../_services/account.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  loggedIn: boolean = false;
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService
+  ) {}
 
   ngOnInit(): void {
-    
+
   }
 
   public login(): void {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log('response', response);
-        this.loggedIn = true;
       },
       error: error => console.log(error)
-    })
+    });
   }
 
   public logout(): void {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 }
