7eb80d3d68ebf169e3b590734a7e7f7dbefd22ca
Jade <hendricksjade@gmail.com>
2023-11-26 16:08:41 +0200

    refactor(extensions): Adding extension methods for the build.sever and program.cs file.

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+    {   
+        services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => 
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true, //must be a token signed by the issue not a randomly generated token key
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+        return services;
+    }
+}
