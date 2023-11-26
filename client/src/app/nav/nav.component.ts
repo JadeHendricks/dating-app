929f6d8e8d940057f2b27e74dfd8ab64b7c5d9f2
Jade <hendricksjade@gmail.com>
2023-11-26 16:57:44 +0200

    add(auth): adding in basic authentication and checks to change the UI based on logged in status.

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn: boolean = false;
 
+  constructor(private accountService: AccountService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  public login(): void {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log('response', response);
+        this.loggedIn = true;
+      },
+      error: error => console.log(error)
+    })
+  }
+
+  public logout(): void {
+    this.loggedIn = false;
+  }
 }
