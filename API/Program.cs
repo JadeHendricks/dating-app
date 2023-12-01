32e54d5151ae421cbcbc9b34f065f41caf85fa20
Jade <hendricksjade@gmail.com>
2023-12-01 14:55:47 +0200

    add(errors): creating an error controller, handling server erros and implementing a exception handling middleware for Dev and Production.

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(CorsPolicyBuilder => CorsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthentication(); // DO YOU HAVE A AVALID TOKEN
 app.UseAuthorization(); // YOU HAVE A VALID TOKEN, WHAT ARE YOU ALLOWED TO DO
