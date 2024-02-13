fa73bfa1823b255ae837eccf141e58b287bee03f
Jade <hendricksjade@gmail.com>
2024-02-13 16:22:02 +0200

    add(messages): using query params and route resolvers to push users to the messages tabs if they are looking to message a potential match..

---
+++ client/src/app/_resolvers/member-detailed.resolver.ts
@@ -0,0 +1,11 @@
+import { ResolveFn } from '@angular/router';
+import { Member } from '../_models/member';
+import { MembersService } from '../_services/members.service';
+import { inject } from '@angular/core';
+
+//a way for routes to get a hold of data before the component is constructed
+export const memberDetailedResolver: ResolveFn<Member> = (route, state) => {
+  const memberService = inject(MembersService);
+
+  return memberService.getMember(route.paramMap.get('username')!);
+};
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
-import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TabDirective, TabsModule, TabsetComponent } from 'ngx-bootstrap/tabs';
 import { TimeagoModule } from 'ngx-timeago';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 import { MemberMessagesComponent } from '../member-messages/member-messages.component';
+import { MessageService } from 'src/app/_services/message.service';
+import { Message } from 'src/app/_models/message';
 
 @Component({
   selector: 'app-member-details',
@@ -17,28 +19,50 @@ import { MemberMessagesComponent } from '../member-messages/member-messages.comp
 })
 export class MemberDetailsComponent implements OnInit {
 
-  public member: Member | undefined;
+  @ViewChild('memberTabs', {static: true}) memberTabs?: TabsetComponent; //static means our memberTbas should be constructed immediately
+  public member: Member = {} as Member;
   public images: GalleryItem[] = [];
+  activeTab?: TabDirective;
+  messages: Message[] = [];
 
   constructor(
-    private memberService: MembersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private messageService: MessageService
   ) {}
 
   ngOnInit(): void {
-    this.loadMember();
-  }
+    this.route.data.subscribe({
+      next: data => this.member = data['member']
+    });
 
-  private loadMember(): void {
-    var username = this.route.snapshot.paramMap.get('username');
-    if (!username) return;
-
-    this.memberService.getMember(username).subscribe({
-      next: (member: Member) => {
-        this.member = member;
-        this.getImages();
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab'] && this.selectTab(params['tab']);
       }
     });
+
+    this.getImages();
+  }
+
+  selectTab(heading: string): void {
+    if (this.memberTabs) {
+      this.memberTabs.tabs.find(x => x.heading === heading)!.active = true;
+    }
+  }
+
+  public onTabActivated(data: TabDirective): void {
+    this.activeTab = data;
+    if (this.activeTab.heading === 'Messages') {
+      this.loadMessages();
+    }
+  }
+
+  public loadMessages(): void {
+    if (this.member) {
+      this.messageService.getMessageThread(this.member.userName).subscribe({
+        next: messages => this.messages = messages
+      });
+    }
   }
 
   private getImages(): void {
